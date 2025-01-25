72d6ee3d07021eb25d7887f2d592faa65fee0c2c
Mimito164 <fjaralampidis@gmail.com>
2025-01-25 01:19:55 -0300

    wip

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import PIL
 import PIL.ImageFile
-import cv2
+import colorsys
 import sys
 import numpy as np
 
@@ -22,50 +22,32 @@ def open_image():
         print(f"Error: Unable to load image at '{img_path}'")
         sys.exit(1)
 
-def detect_pixels(img:PIL.ImageFile):
+def detect_colors(img:PIL.ImageFile):
     px = img.load()
     width,height = img.size
     for i in range(width):
         for j in range(height):
-            print(px[i,j])
+            h,s,v = px[i,j]
+            
+            # print(f"h:{int(h)}\ts:{int(s)}\tv:{int(v)}")
+            color = (int(h),int(s),int(v))
+            if f"{color}" in detected_colors:
+                detected_colors[f"{color}"] += 1
+            else:
+                detected_colors[f"{color}"] = 1
+
+
 
 
 
 def main():
     img = open_image()    
-    detect_pixels(img)
+    detect_colors(img)
     
-    
-    # hls_img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
-
-    # width, height, channel = hls_img.shape
-    # h_values, l_values, s_values = cv2.split(hls_img)
-  
-    # print(np.unique(h_values))
-
-    # for i in range(width):
-    #     for j in range(height):
-            
-    #         h,l,s = hls_img[i][j][0], hls_img[i][j][1], hls_img[i][j][2]
-    #         color_tuple = (h,l,s)
-    #         color_key = f"{color_tuple}" 
-    #         if color_key in color_count:
-    #             color_count[color_key] += 1
-    #         else:
-    #             color_count[color_key] = 1
-
-    # print(color_count)
-    # for i in range(hls_img):
-    #     for j in row:
-    #         h,l,s = hls_img[]
-    #         print(pixel, end=' ')
-
-
-
-    # cv2.imshow("Logo", hls_img)
-    # cv2.waitKey(0)  # Wait indefinitely for a key press
-    # cv2.destroyAllWindows()
-
+    # print(detected_colors)
+    sorted_items = sorted(detected_colors.items(), key=lambda item: item[1], reverse=True)
+    for color in sorted_items:
+        print(color,end='\n')
 
 if __name__ == "__main__":
     main()
